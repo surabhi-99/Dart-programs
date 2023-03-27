83e4fcbcc524e9e1bd609872f12a0e756fe72e83
Surabhi Thakur <61082171+surabhi-99@users.noreply.github.com>
2023-03-27 22:46:37 +0530

    Create hello-list.dart

---
+++ hello-list.dart
@@ -0,0 +1,6 @@
+void main(){
+  List<String> namesOfTheWeek = ['mon', 'tues', 'wed', 'thurs', 'fri'];
+  print ('Hello $namesOfTheWeek');
+  print ('Hello $namesOfTheWeek[0]');
+  print ('Hello $namesOfTheWeek[${namesOfTheWeek.length}]');
+}
