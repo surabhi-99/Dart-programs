82df19730d4618639bdd05e457450c8bc32163f4
Surabhi Thakur <61082171+surabhi-99@users.noreply.github.com>
2023-03-27 22:39:12 +0530

    Create hello-function.dart

---
+++ hello-function.dart
@@ -0,0 +1,12 @@
+void main() {
+  bool isDartCool = isDartCoolFunc();
+  if (isDartCool) {
+    print('Hello $isDartCool');
+  } else {
+    print('Hmm I think Dart is pretty cool!');
+  }
+}
+bool isDartCoolFunc() {
+  bool isDartCool = true;
+  return isDartCool;
+}
